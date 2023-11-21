1d2946a548f3e02c64462ab22c0585d07f49f841
libin <libin@douyuxingchen.com>
2023-11-21 14:58:08 +0800

    project init

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    // ./vendor/bin/phpunit --filter testExample ./tests/Unit/ExampleTest.php
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
