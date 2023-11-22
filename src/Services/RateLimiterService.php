a8f424dc2726227212ff29fb318eb74bf9c3fc5c
libin <libin@douyuxingchen.com>
2023-11-22 09:46:51 +0800

    feat：开发签名认证和接口限流

--- src/Services/RateLimiterService.php
+++ src/Services/RateLimiterService.php
@@ -8,10 +8,12 @@ use Douyuxingchen\PhpSysAuth\Exceptions\ErrCodeException;
 class RateLimiterService {
 
     private $limitNumber;
+    private $cache;
 
     public function __construct($limitNumber)
     {
         $this->limitNumber = $limitNumber;
+        $this->cache = BaseCache::getInstance();
     }
 
     /**
@@ -25,20 +27,48 @@ class RateLimiterService {
         }
 
         $key = 'throttle:' . $appKey;
-        $cache = BaseCache::getInstance();
-        $lastRequestTime = $cache->get($key);
+        $lastRequestTime = $this->cache->get($key);
 
         if (!$lastRequestTime) {
-            $cache->set($key, time(), 'EX', 1);
+            $this->cache->set($key, time(), 'EX', 1);
         } else {
             $timeDifference = time() - $lastRequestTime;
             $interval = 1 / $this->limitNumber;
             if ($timeDifference < $interval) {
                 throw new ErrCodeException('Request too frequently', ErrCodeEnums::ERR_REQUEST_FREQUENTLY);
             } else {
-                $cache->set($key, time(), 'EX', 1);
+                $this->cache->set($key, time(), 'EX', 1);
             }
         }
+    }
+
+    /**
+     * @throws ErrCodeException
+     */
+    public function throttle2($appKey)
+    {
+        if ($this->limitNumber == 0) {
+            return;
+        }
+
+        $key = 'throttle:' . $appKey;
+        $requestTimes = $this->cache->get($key);
+
+        if (!$requestTimes) {
+            $requestTimes = [];
+        }
+
+        $currentTime = time();
+        $requestTimes = array_filter($requestTimes, function ($time) use ($currentTime) {
+            return $currentTime - $time < 1; // 限制在1秒内的请求记录
+        });
+
+        if (count($requestTimes) >= $this->limitNumber) {
+            throw new ErrCodeException('Request too frequently', ErrCodeEnums::ERR_REQUEST_FREQUENTLY);
+        }
+
+        $requestTimes[] = $currentTime;
+        $this->cache->set($key, $requestTimes);
     }
 
 }
